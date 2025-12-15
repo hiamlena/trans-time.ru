77da9d47e8971aebef3aa34ad7511064a1d80e88
hiamlena <helena_vinokurova@icloud.com>
2025-12-15 11:45:48 +0300

    API: add passenger_wsgi entrypoint

--- api/passenger_wsgi.py
+++ api/passenger_wsgi.py
@@ -1,11 +1,13 @@
-# /api/passenger_wsgi.py
 import os
 import sys
 
+# Путь к папке /api/backend внутри репозитория
 BASE_DIR = os.path.dirname(__file__)
 BACKEND_DIR = os.path.join(BASE_DIR, "backend")
 
+# Чтобы работали импорты: from db import ... / from models import ...
 if BACKEND_DIR not in sys.path:
     sys.path.insert(0, BACKEND_DIR)
 
-from app_flask import app as application
+# Подхватываем Flask-приложение
+from app_flask import app as application  # noqa
